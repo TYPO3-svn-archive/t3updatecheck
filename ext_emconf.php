dfac56b9ab6d4b6866069741492664249f9833bd
surffritze <surffritze@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-16 17:22:36 +0000

    added translation for table-fields;
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3updatecheck/trunk@19079 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "t3updatecheck"
 #
-# Auto generated 23-03-2009 17:25
+# Auto generated 16-04-2009 19:19
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'alpha',
 	'internal' => '',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Visia GmbH',
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"5296";s:21:"ext_conf_template.txt";s:4:"f49b";s:12:"ext_icon.gif";s:4:"d615";s:17:"ext_localconf.php";s:4:"52e7";s:14:"ext_tables.php";s:4:"55c2";s:14:"ext_tables.sql";s:4:"3073";s:16:"locallang_db.php";s:4:"d41d";s:7:"tca.php";s:4:"95ee";s:34:"cli/class.tx_t3updatecheck_cli.php";s:4:"bdc1";s:14:"doc/manual.sxw";s:4:"f383";s:19:"doc/wizard_form.dat";s:4:"57c0";s:20:"doc/wizard_form.html";s:4:"0db8";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"5296";s:21:"ext_conf_template.txt";s:4:"f49b";s:12:"ext_icon.gif";s:4:"d615";s:17:"ext_localconf.php";s:4:"52e7";s:14:"ext_tables.php";s:4:"55c2";s:14:"ext_tables.sql";s:4:"3073";s:33:"icon_tx_t3updatecheck_updates.gif";s:4:"d615";s:16:"locallang_db.xml";s:4:"9538";s:7:"tca.php";s:4:"95ee";s:34:"cli/class.tx_t3updatecheck_cli.php";s:4:"d8b1";s:17:"cli/locallang.xml";s:4:"94b3";s:14:"doc/manual.sxw";s:4:"8f2b";s:19:"doc/wizard_form.dat";s:4:"57c0";s:20:"doc/wizard_form.html";s:4:"0db8";}',
 	'suggests' => array(
 	),
 );
