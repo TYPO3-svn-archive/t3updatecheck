6e3bd010fe296c3e1d26ba9b99ac4f1cb3e2ecfe
surffritze <surffritze@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-22 15:49:37 +0000

    added warning messages to "About Modules" if there are new TYPO3 versions; you can now choose if the extension should look for stable, development or both kinds of releases;
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3updatecheck/trunk@19341 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_t3updatecheck_befuncdispwm.php
+++ class.tx_t3updatecheck_befuncdispwm.php
@@ -2,12 +2,19 @@
 class tx_t3updatecheck_befuncdispwm {
 	
 	function displayWarningMessages_postProcess (&$warnings) {
-		/*
-		$warnings["typo3_update"] = sprintf(
-			'<a href="http://www.typo3.org/downloads/">',
-			'Download the latest TYPO3 Version',
-			'</a>');
-		*/
+		
+		// check for higher versions in tx_t3updatecheck_updates table
+		// will not inform you about alpha and beta versions as they are no important updates
+		$where_clause = 'version > '.TYPO3_version.' AND version NOT LIKE "%alpha%" AND version NOT LIKE "%beta%"';
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('version', 'tx_t3updatecheck_updates', $where_clause);
+		
+		if(mysql_num_rows($res)) {
+			$warnings["typo3_update"] = sprintf(
+				$GLOBALS['LANG']->sL('LLL:EXT:t3updatecheck/locallang.xml:warning.typo3_update'),
+				'<a href="http://www.typo3.org/downloads/" target="_blank">',
+				'</a>');
+		}
+		
 	}
 	
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "t3updatecheck"
 #
-# Auto generated 16-04-2009 19:31
+# Auto generated 22-04-2009 17:48
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TYPO3 Update Check',
-	'description' => 'This TYPO3 Extension checks for available TYPO3 Updates and will inform you by email.',
+	'description' => 'This extension makes it possible to automatically check for new TYPO3 updates against the sourceforge downloadable files. It will inform via email, if there are new updates available. Works with Cronjob (TYPO3 CLI).',
 	'category' => 'be',
 	'author' => 'Ole Fritz',
 	'author_email' => 'ole.fritz@visia.de',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Visia GmbH',
-	'version' => '0.0.4',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"5296";s:21:"ext_conf_template.txt";s:4:"f49b";s:12:"ext_icon.gif";s:4:"d615";s:17:"ext_localconf.php";s:4:"52e7";s:14:"ext_tables.php";s:4:"55c2";s:14:"ext_tables.sql";s:4:"3073";s:33:"icon_tx_t3updatecheck_updates.gif";s:4:"d615";s:16:"locallang_db.xml";s:4:"9538";s:7:"tca.php";s:4:"95ee";s:34:"cli/class.tx_t3updatecheck_cli.php";s:4:"e003";s:14:"doc/manual.sxw";s:4:"8f2b";s:19:"doc/wizard_form.dat";s:4:"57c0";s:20:"doc/wizard_form.html";s:4:"0db8";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"5296";s:39:"class.tx_t3updatecheck_befuncdispwm.php";s:4:"6859";s:21:"ext_conf_template.txt";s:4:"7043";s:12:"ext_icon.gif";s:4:"d615";s:17:"ext_localconf.php";s:4:"a0db";s:14:"ext_tables.php";s:4:"55c2";s:14:"ext_tables.sql";s:4:"3073";s:33:"icon_tx_t3updatecheck_updates.gif";s:4:"d615";s:13:"locallang.xml";s:4:"20e8";s:16:"locallang_db.xml";s:4:"9538";s:7:"tca.php";s:4:"95ee";s:34:"cli/class.tx_t3updatecheck_cli.php";s:4:"783d";s:14:"doc/manual.sxw";s:4:"8f2b";s:19:"doc/wizard_form.dat";s:4:"57c0";s:20:"doc/wizard_form.html";s:4:"0db8";}',
 	'suggests' => array(
 	),
 );
