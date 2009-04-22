76dcb5249b8f5559e0e9665b1cf4bfaef14a60a3
surffritze <surffritze@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-22 00:47:26 +0000

    working on warning-message for about modules (not ready yet);
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3updatecheck/trunk@19319 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_t3updatecheck_befunc.php
@@ -0,0 +1,14 @@
+<?php
+class tx_t3updatecheck_befuncdispwm {
+	
+	function displayWarningMessages_postProcess (&$warnings) {
+		/*
+		$warnings["typo3_update"] = sprintf(
+			'<a href="http://www.typo3.org/downloads/">',
+			'Download the latest TYPO3 Version',
+			'</a>');
+		*/
+	}
+	
+}
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,7 @@
 <?php
+  ## Setting up script to display warning messages in About Modules
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['displayWarningMessages'][] = 'EXT:'.$_EXTKEY.'/class.tx_t3updatecheck_befuncdispwm.php/:tx_t3updatecheck_befuncdispwm';
+
   ## Setting up script that can be run through cli_dispatch.phpsh
 if (TYPO3_MODE == 'BE') {
 	$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array('EXT:'.$_EXTKEY.'/cli/class.tx_t3updatecheck_cli.php','_CLI_cronjob');
