40609d7a6cb2c4d9e8b80460a3d994309b92bd7a
surffritze <surffritze@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-23 11:36:52 +0000

    [BUGFIX] Fixed a bug with displayWarningMessages;
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3updatecheck/trunk@19365 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_t3updatecheck_befuncdispwm.php
+++ class.tx_t3updatecheck_befuncdispwm.php
@@ -5,7 +5,7 @@ class tx_t3updatecheck_befuncdispwm {
 		
 		// check for higher versions in tx_t3updatecheck_updates table
 		// will not inform you about alpha and beta versions as they are no important updates
-		$where_clause = 'version > '.TYPO3_version.' AND version NOT LIKE "%alpha%" AND version NOT LIKE "%beta%"';
+		$where_clause = 'version > "'.TYPO3_version.'" AND version NOT LIKE "%alpha%" AND version NOT LIKE "%beta%"';
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('version', 'tx_t3updatecheck_updates', $where_clause);
 		
 		if(mysql_num_rows($res)) {
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "t3updatecheck"
 #
-# Auto generated 23-04-2009 02:15
+# Auto generated 23-04-2009 12:41
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Visia GmbH',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"5296";s:39:"class.tx_t3updatecheck_befuncdispwm.php";s:4:"6859";s:21:"ext_conf_template.txt";s:4:"a127";s:12:"ext_icon.gif";s:4:"d615";s:17:"ext_localconf.php";s:4:"a0db";s:14:"ext_tables.php";s:4:"55c2";s:14:"ext_tables.sql";s:4:"3073";s:33:"icon_tx_t3updatecheck_updates.gif";s:4:"d615";s:13:"locallang.xml";s:4:"20e8";s:16:"locallang_db.xml";s:4:"9538";s:7:"tca.php";s:4:"95ee";s:34:"cli/class.tx_t3updatecheck_cli.php";s:4:"6ab9";s:14:"doc/manual.sxw";s:4:"318d";s:19:"doc/wizard_form.dat";s:4:"57c0";s:20:"doc/wizard_form.html";s:4:"0db8";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"5296";s:39:"class.tx_t3updatecheck_befuncdispwm.php";s:4:"ad77";s:21:"ext_conf_template.txt";s:4:"7043";s:12:"ext_icon.gif";s:4:"d615";s:17:"ext_localconf.php";s:4:"6531";s:14:"ext_tables.php";s:4:"55c2";s:14:"ext_tables.sql";s:4:"3073";s:33:"icon_tx_t3updatecheck_updates.gif";s:4:"d615";s:13:"locallang.xml";s:4:"20e8";s:16:"locallang_db.xml";s:4:"9538";s:7:"tca.php";s:4:"95ee";s:34:"cli/class.tx_t3updatecheck_cli.php";s:4:"6ab9";s:14:"doc/manual.sxw";s:4:"318d";s:19:"doc/wizard_form.dat";s:4:"57c0";s:20:"doc/wizard_form.html";s:4:"0db8";}',
 	'suggests' => array(
 	),
 );
